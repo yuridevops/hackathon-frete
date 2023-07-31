8e513d88f6845ec94b96d47eb951237a62c0ac39
Marcos Yuri <yuri.devops@gmail.com>
2023-07-30 22:09:33 -0400

    fix: useEffect twice call

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,7 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
 import '@frete.com/fuel-fonts/satoshi/fontface.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+root.render(<App />)
